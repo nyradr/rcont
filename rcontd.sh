b59ed27d0986597bf7d6ea03f2f35e10f2c096a6
nyradr <nyradr@protonmail.com>
2016-07-01 21:21:24 +0200

    add LSB tags

--- rcontd.sh
+++ rcontd.sh
@@ -1,5 +1,15 @@
 #!bin/sh
 
+### BEGIN INIT INFO
+# Provides:			rcont startup
+# Required-Start:	$syslog
+# Required-Stop:	$syslog
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description: start rcont
+# Description:		start rcont daemon (relay manager)
+### END INIT INFO
+
 SERVICE_NAME="rcont"
 
 DIR=/home/pi/git/rcont

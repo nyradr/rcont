4ac6a52f75d3b173cea6e36d1239bbaac2f2d0ff
nyradr <nyradr@protonmail.com>
2016-07-06 17:59:41 +0200

    bug fix

--- rcont.c
+++ rcont.c
@@ -18,7 +18,7 @@ void rcont_log(const char* mess, ...){
 	FILE* file = fopen(RCONT_LOG, "a");
 	
 	if(file){
-		va_arg args;
+		va_list args;
 		va_start(args, mess);
 		
 		fprintf(file, mess, args);

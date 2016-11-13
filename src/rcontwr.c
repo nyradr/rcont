1abb40773ae6a794524401165ca07d1a3f1ffab2
nyradr <nyradr@protonmail.com>
2016-11-13 18:43:46 +0100

    some fix in rcontwr

--- src/rcontwr.c
+++ src/rcontwr.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -16,16 +17,21 @@
    delay : delay before switch
  */
 void write_relay(int relay, int delay){
+  if(relay <= 0){
+    printf("Invalid relay");
+    return;
+  }
+  
   // init relay file
   char buff[BSIZE] = {0};
   if(snprintf(buff, BSIZE, "%s%d.in", DIR, relay) > 0){
 
     // open FIFO file
     int fd = open(buff, O_WRONLY);
-    if(fd < 0){
+    if(fd >= 0){
 
       //write data
-      if(write(fd, &delay, sizeof(int)) == sizeof(int))
+      if(write(fd, &delay, sizeof(int)) > 0)
 	printf("Success\n");
       else
 	printf("Error in delay write\n");
@@ -41,23 +47,14 @@ int main(int argc, char** argv){
   char buff[BSIZE] = {0};
   FILE* file;
 	
-  switch(argc){
-  case 3:
-    snprintf(buff, BSIZE, "%s%s.in", DIR, argv[1]);
-			
-    file = fopen(buff, "a");
-    if(file){
-      fprintf(file, "\n%s", argv[2]);
-      fclose(file);
-    }else
-      printf("Impossible to open the file : %s", buff);
-		
-    break;
-			
-  default:
+  if(argc == 3){
+    int relay = strtol(argv[1], NULL, 10);
+    int delay = strtol(argv[2], NULL, 10);
+    write_relay(relay, delay);    
+  }else{
     printf("Invalid arguments : must be \"rcontwr relay delay\"\n");
     printf("\trelay from 1 - nrelays\n");
-    printf("\tdelay delay before reset or 0\n");
-    break;
+    printf("\tdelay delay before switch, 0 for instant switch\n");
+    printf("\tnegative delay for reset or all commands\n");
   }
 }

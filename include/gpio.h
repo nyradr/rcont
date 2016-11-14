7fb756f26cbb2430bee9060af48333c9a8ff7458
nyradr <nyradr@protonmail.com>
2016-11-14 16:02:18 +0100

    change GPIO (no more exit on error)

--- include/gpio.h
+++ include/gpio.h
@@ -3,22 +3,22 @@
 
 /* Open gpio port in output mode
    pin : GPIO pin name
-   return : 0 in case of errors
-   //TODO : value test (< 0)
+   return : 1 if no error
+            0 if an error occur
 */
 char gpio_init(int pin);
 
 /* Close gpio port
    pin : GPIO pin name
-   return : 0 in case of error
-   //TODO : value test (< 0)
+   return : 1 if no error
+            0 if an error occur
 */
 char gpio_close(int pin);
 
 /* Write value in GPIO port
    pin : GPIO pin name
-   return : 0 in case of error
-   //TODO : value test (< 0)
+   return : 1 if no error
+            1 if an error occur
 */
 char gpio_write(int pin, char val);
 
